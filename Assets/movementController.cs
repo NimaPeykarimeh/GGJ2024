c941c80eb49538f9f6f166aa6ed95011aa059122
NimaPeykarimeh <nimapeykerimeh87@gmail.com>
2024-01-26 21:51:53 +0300

    playerMovement

---
+++ Assets/movementController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    Vector3 moveDirection;
+
+    Rigidbody playerRb;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+        playerRb.velocity = moveDirection * movementSpeed;
+    }
+}
